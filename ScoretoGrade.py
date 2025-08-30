cbdf14304688363b7e6b032e0ff6195066a67f5d
Daniel Harris <daniel.t.harris@gmail.com>
2025-08-29 21:33:11 -0700

    Add files via upload
    Conditional Execution
    3.3 Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error. If the score is between 0.0 and 1.0, print a grade using the following table:
    Score Grade
    >= 0.9 A
    >= 0.8 B
    >= 0.7 C
    >= 0.6 D
    < 0.6 F
    If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.

---
+++ ScoretoGrade.py
@@ -0,0 +1,19 @@
+# This program converts a score between 0.00 and 1.00 into a letter grade
+while True:
+    scorei = input("Enter score between 0.00 and 1.00: ")
+    score = float(scorei)
+    if score >= 0.0 and score <= 1.0:
+        if score >= 0.9:
+            print("The grade for {score} is an A")
+        elif score >= 0.8:
+            print("The grade for {score} is a B")
+        elif score >= 0.7:
+            print("The grade for {score} is a C")
+        elif score >= 0.6:
+            print("The grade for {score} is a D")
+        else:
+            print("The grade for {score} is an F")
+        break
+    else:
+        print("Error, the score entered was not between 0.00 and 1.00.")
+quit()
