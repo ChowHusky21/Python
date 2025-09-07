5659492ba44246f29f072aa08e34af364694fc8e
Daniel Harris <daniel.t.harris@gmail.com>
2025-09-07 09:03:50 -0700

    Add files via upload
    A boolean expression is an expression that is either true or false. There are three logical operators: and, or, and not. Conditional statements gives us the ability to write useful programs where conditions can be checked and the behavior of the program changed accordingly.
    
    Exercise 4.6: Rewrite your pay computation with time-and-a-half for overtime and create a function called computepay which takes two parameters (hours and rate).
    Severance, C. (2016). Python for Everybody: Exploring Data Using Python 3. a Creative Commons Attribution.

--- CalGroPayfunctions.py
+++ CalGroPayfunctions.py
@@ -1,14 +1,20 @@
 # This function clears the terminal screen
 import os
 os.system('cls' if os.name == 'nt' else 'clear')
-# This program calculates gross pay to demonstrate the use of functions
-h = float(input('Enter Hours: '))
-r = float(input('Enter Rate: '))
+# Exercise 4.6: Rewrite your pay computation with time-and-a-half for overtime and
+# create a function called computepay which takes two parameters (hours and rate).
+# Severance, C. (2016). Python for Everybody: Exploring Data Using Python 3. a Creative Commons Attribution.
+print ('This program calculates gross pay to /n demonstrate the use of functions.')
+h = float(
+    input('Enter Hours: '))
+r = float(
+    input('Enter Rate: '))
 def computepay (h, r):
     return h * r
 if h > 40:
     def ot (h, r):
         return (h - 40) * (r * 1.5)
+
     print (f'Pay {((computepay(40, r)) + ot(h, r)):.2f}')
 else:
     print (f'Pay {computepay(h, r):.2f}')
