19036156ca39647ecb057febe45804771fc2d442
Daniel Harris <daniel.t.harris@gmail.com>
2025-09-07 09:00:39 -0700

    Add files via upload
    A value is one of the basic things a program works with, like a letter or a number.  A variable is a name that refers to a value. An assignment statement creates new variables and gives them values. A statement is a unit of code that the Python interpreter can execute.
    
    Exercise 2.3: Write a program to prompt the user for hours and rate per hour to compute gross pay.
    Severance, C. (2016). Python for Everybody: Exploring Data Using Python 3. a Creative Commons Attribution.

---
+++ CalGroPaystatements.py
@@ -0,0 +1,10 @@
+# This code will calculate total pay earned.
+# Exercise 2.3: Write a program to prompt the user for hours and rate per hour to
+# compute gross pay.
+# Severance, C. (2016). Python for Everybody: Exploring Data Using Python 3. a Creative Commons Attribution.
+print ('This program calculates gross pay to /n demonstrate the use of variables, expressions, and statements.')
+h = float(
+    input('Enter Hours: '))
+r = float(
+    input('Enter Rate: '))
+print(f'Pay: {(hours * rate):.2f}')
